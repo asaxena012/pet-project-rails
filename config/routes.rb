04f8675c36494603b72549c889ec3ad39ab6f331
Aditya Saxena <60032144+asaxena012@users.noreply.github.com>
2022-12-21 06:48:25 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'pets/index'
+  get 'pets/show'
+  get 'pets/edit'
+  get 'pets/new'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
