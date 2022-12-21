e97a3376224bdca0d7d248c55ad68e3d9fde74b5
Aditya Saxena <60032144+asaxena012@users.noreply.github.com>
2022-12-21 06:59:40 +0000

    list pets resource added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'pets/index'
-  get 'pets/show'
-  get 'pets/edit'
-  get 'pets/new'
+  # get 'pets/index'
+  # get 'pets/show'
+  # get 'pets/edit'
+  # get 'pets/new'
+  resources :pets
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pets#index"
 end
